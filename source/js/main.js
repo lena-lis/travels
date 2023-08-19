25dd73f51ee10c07f0061fdf8eb7a442330808de
Elena <fjernt.land@gmail.com>
2023-08-19 14:56:59 +0300

    feat(header): доб. стили планшетов, скрипт меню

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,6 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {Form} from './modules/form-validate/form';
+import {menuToggle} from './modules/menu-toggle/menu-toggle';
 
 // ---------------------------------
 
@@ -16,6 +17,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
+    menuToggle();
     const form = new Form();
     window.form = form;
     form.init();
---
+++ source/js/modules/menu-toggle/menu-toggle.js
@@ -0,0 +1,25 @@
+const menuToggle = () => {
+  const navMenuOverlay = document.querySelector('[data-menu-overlay]');
+  const navMain = document.querySelector('[data-nav-main]');
+  const navMenu = document.querySelector('[data-nav-menu]');
+  const navToggle = document.querySelector('[data-nav-toggle]');
+
+  if (navMenuOverlay && navMain && navMenu && navToggle) {
+    navMain.classList.remove('main-navigation--nojs');
+    navMain.classList.add('main-navigation--closed');
+
+    navToggle.addEventListener('click', function () {
+      if (navMain.classList.contains('main-navigation--closed')) {
+        navMain.classList.remove('main-navigation--closed');
+        navMain.classList.add('main-navigation--opened');
+        navMenuOverlay.style.display = 'block';
+      } else {
+        navMain.classList.add('main-navigation--closed');
+        navMain.classList.remove('main-navigation--opened');
+        navMenuOverlay.style.display = 'none';
+      }
+    });
+  }
+};
+
+export {menuToggle};
