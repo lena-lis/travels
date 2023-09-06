d7c092eef2cb8ac479895c2d97ec0b52b237432d
Elena Lisichenok <111569149+lena-lis@users.noreply.github.com>
2023-09-06 21:10:49 +0300

    fix(hero)

--- source/js/modules/slider/hero-slider.js
+++ source/js/modules/slider/hero-slider.js
@@ -1,5 +1,18 @@
 import Swiper from '../../vendor/swiper.js';
 
+function catchActiveSlideFocus() {
+  const heroSlides = document.querySelectorAll('.hero__item');
+  const activeHeroSlide = document.querySelector('.hero__item.swiper-slide-active');
+
+  if (!activeHeroSlide) {
+    return;
+  }
+
+  const focusableSlides = Array.from(heroSlides).filter((heroSlide) => !heroSlide.classList.contains('swiper-slide-active'));
+  focusableSlides.forEach((element) => element.setAttribute('inert', true));
+  activeHeroSlide.removeAttribute('inert');
+}
+
 const initHeroSlider = new Swiper('[data-hero-slider]', {
   loop: true,
   speed: 300,
@@ -15,16 +28,3 @@ const initHeroSlider = new Swiper('[data-hero-slider]', {
 });
 
 initHeroSlider.on('afterInit', catchActiveSlideFocus());
-
-function catchActiveSlideFocus() {
-  const heroSlides = document.querySelectorAll('.hero__item');
-
-  if (!activeHeroSlide) {
-    return;
-  }
-
-  const focusableSlides = Array.from(heroSlides).filter((heroSlide) => !heroSlide.classList.contains('swiper-slide-active'));
-  focusableSlides.forEach((element) => element.setAttribute('inert', true));
-  const activeHeroSlide = document.querySelector('.hero__item.swiper-slide-active');
-  activeHeroSlide.removeAttribute('inert');
-}
